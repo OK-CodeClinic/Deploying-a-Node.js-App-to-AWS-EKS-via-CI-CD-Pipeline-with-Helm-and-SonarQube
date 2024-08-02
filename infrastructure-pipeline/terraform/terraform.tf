c570f3d1ad078605214b95caa7c04508b167b3c8
Kehinde Omokungbe <kehindefranklin@gmail.com>
2024-08-02 11:56:41 +0100

    Pipeline

---
+++ infrastructure-pipeline/terraform/terraform.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = "~> 1.1" # This includes all versions from 1.1.0 to < 2.0.0
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.25.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "weather-app-gitops-bucket"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
